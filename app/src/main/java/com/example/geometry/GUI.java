d010b856521c9ea3910e901a1a7c9a87d8123335
ruca905 <yarkivaev@mail.ru>
2020-04-04 21:11:03 +0700

    Add moving lines

--- app/src/main/java/com/example/geometry/GUI.java
+++ app/src/main/java/com/example/geometry/GUI.java
@@ -20,6 +20,11 @@ public class GUI extends View {
     Line tempLine;
     Node startTempNode;
     Node stopTempNode;
+    Line currentLine;
+    Node currentNode;
+    float lastXTouch;
+    float lastYTouch;
+    public static int delta = 25;
 
 
     public GUI(Context context) {
@@ -37,9 +42,9 @@ public class GUI extends View {
     public void onDraw(Canvas canvas) {
         super.onDraw(canvas);
         mPaint.setColor(Color.GRAY);
-        canvas.drawLine(10, 0, 10, 100, mPaint);
         for (Line line:figure.lines) {
             canvas.drawLine(line.start.x, line.start.y, line.stop.x, line.stop.y, mPaint);
+            //canvas.drawPath(line.mPath, mPaint);
         }
         for (Node node:figure.nodes) {
             canvas.drawCircle(node.x, node.y, 10, mPaint);
@@ -52,30 +57,65 @@ public class GUI extends View {
         float mx = event.getX();
         float my = event.getY();
 
-        for (Line line : figure.lines) {
-            if (line.r.contains((int) mx, (int) my)) {
-                Log.d("hello", "hello");
-            }
-        }
+
         switch (event.getAction()) {
             case MotionEvent.ACTION_DOWN:
-
-                startTempNode = new Node(mx, my);
-                stopTempNode = new Node(mx, my);
-                tempLine = new Line(startTempNode, stopTempNode);
-                figure.nodes.add(startTempNode);
-                figure.nodes.add(stopTempNode);
+                currentNode = null;
+                currentLine = null;
+                for (Node node : figure.nodes) {
+                    if (Math.pow((mx - node.x), 2) + Math.pow((my - node.y), 2)  <= delta*delta){
+                        currentNode = node;
+                        break;
+                    }
+                }
+                if (currentNode == null) {
+                    for (Line line : figure.lines) {
+                        if (line.r.contains((int) mx, (int) my)) {
+                            currentLine = line;
+                            lastXTouch = mx;
+                            lastYTouch = my;
+                            break;
+                        }
+                    }
+                }
+                if (currentNode == null && currentLine == null) {
+                    startTempNode = new Node(mx, my);
+                    stopTempNode = new Node(mx, my);
+                    tempLine = new Line(startTempNode, stopTempNode);
+                    figure.nodes.add(startTempNode);
+                    figure.nodes.add(stopTempNode);
+                }
                 invalidate();
 
             case MotionEvent.ACTION_MOVE:
-                stopTempNode.setXY(mx, my);
-                tempLine.setStop(stopTempNode);
+                if (currentNode != null){
+                    currentNode.setXY(mx, my);
+                }
+                if (currentLine != null) {
+                    float deltaX = mx - lastXTouch;
+                    float deltaY = my - lastYTouch;
+                    currentLine.setXYNodes(currentLine.stop.x + deltaX, currentLine.stop.y + deltaY,
+                            currentLine.start.x + deltaX, currentLine.start.y + deltaY);
+                    lastXTouch = mx;
+                    lastYTouch = my;
+                }
+                if (currentNode == null && currentLine == null) {
+                    stopTempNode.setXY(mx, my);
+                    tempLine.setStop(stopTempNode);
+                    startTempNode.addLine(tempLine);
+                }
                 invalidate();
 
             case MotionEvent.ACTION_UP:
-                stopTempNode.setXY(mx, my);
-                tempLine.setStop(stopTempNode);
-                figure.lines.add(tempLine);
+                if (currentNode != null){
+                    currentNode.setXY(mx, my);
+                }
+                if (currentNode == null && currentLine == null) {
+                    stopTempNode.setXY(mx, my);
+                    stopTempNode.addLine(tempLine);
+                    tempLine.setStop(stopTempNode);
+                    figure.lines.add(tempLine);
+                }
                 invalidate();
         }
         return true;
--- app/src/main/java/com/example/geometry/Node.java
+++ app/src/main/java/com/example/geometry/Node.java
@@ -1,17 +1,38 @@
 package com.example.geometry;
 
+import java.util.ArrayList;
+
 public class Node{
     public float x;
     public float y;
+    ArrayList<Line> lines = new ArrayList<>();
 
     public Node(float x, float y) {
         this.x = x;
         this.y = y;
     }
 
+    public Node(float x, float y, Line line) {
+        this.x = x;
+        this.y = y;
+        lines.add(line);
+    }
+
+    public Node(float x, float y, ArrayList<Line> lines) {
+        this.x = x;
+        this.y = y;
+        this.lines = lines;
+    }
 
     public void setXY(float x, float y) {
         this.x = x;
         this.y = y;
+        for (Line line: lines) {
+            line.createPathFromLine();
+        }
+    }
+
+    public void addLine(Line line) {
+        lines.add(line);
     }
 }
