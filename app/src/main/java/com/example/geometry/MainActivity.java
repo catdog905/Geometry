6fe0be0776b54f6715cc26e90c75f13490ac8375
Michail <mlarshin@ya.ru>
2020-04-10 02:11:18 +0300

    bigger back button & some debug

--- app/src/main/java/com/example/geometry/MainActivity.java
+++ app/src/main/java/com/example/geometry/MainActivity.java
@@ -3,7 +3,6 @@ package com.example.geometry;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
-import android.widget.Button;
 import android.widget.ImageButton;
 
 import androidx.appcompat.app.AppCompatActivity;
@@ -19,6 +18,7 @@ public class MainActivity extends AppCompatActivity {
         solve_button.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+//                 RuleBase.solve(DrawFragment.drawCanvas);
                 Intent intent = new Intent(MainActivity.this, SolveActivity.class);
                 startActivity(intent);
             }
--- app/src/main/java/com/example/geometry/SolveActivity.java
+++ app/src/main/java/com/example/geometry/SolveActivity.java
@@ -1,11 +1,10 @@
 package com.example.geometry;
 
-import androidx.appcompat.app.AppCompatActivity;
-
-import android.app.Activity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.ImageButton;
+
+import androidx.appcompat.app.AppCompatActivity;
 
 public class SolveActivity extends AppCompatActivity {
 
