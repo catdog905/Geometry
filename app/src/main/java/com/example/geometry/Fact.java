ea5f9b3a061276d018ab2399514aed8cc14e61b0
ruca905 <yarkivaev@mail.ru>
2020-07-27 12:33:28 +0700

    build archetiture of solving system and soution presentation

---
+++ app/src/main/java/com/example/geometry/Fact.java
@@ -0,0 +1,13 @@
+package com.example.geometry;
+
+import java.util.ArrayList;
+
+public class Fact {
+    ArrayList<String> conditions;
+    ArrayList<String> consequences;
+
+    public Fact(ArrayList<String> conditions, ArrayList<String> consequences) {
+        this.conditions = conditions;
+        this.consequences = consequences;
+    }
+}
---
+++ app/src/main/java/com/example/geometry/MathModel.java
@@ -0,0 +1,11 @@
+package com.example.geometry;
+
+import java.util.ArrayList;
+
+public class MathModel {
+    ArrayList<Fact> globalFacts;
+
+    public MathModel(ArrayList<Fact> globalFacts) {
+        this.globalFacts = globalFacts;
+    }
+}
